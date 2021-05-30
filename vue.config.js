b17f99bd85224e4622449427d63140699dd52586
Cory <Hyakkushiki@users.noreply.github.com>
2021-05-31 00:28:15 +0900

    formatting and spacing

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/tic-tac-toe/'
-      : '/tic-tac-toe/'
+  function getPublicPath() {
+    console.log("*** ___ --- " + process.env.NODE_ENV + "  --- ___ ***");
+    switch (process.env.NODE_ENV) {
+      case 'production': return '/tic-tac-toe/'
+      case 'qa': return '/QaEnv/'
+      case 'dev': return '/DevEnv/'
+      case 'stage': return '/StageEnv/'
+      default: return '/'
+    }
+  }
+  
+  module.exports = {
+    publicPath: getPublicPath()
   }
